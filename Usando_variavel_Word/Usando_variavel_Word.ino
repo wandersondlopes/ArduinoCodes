0af16ac192668f167a3311aa38e7d0799f8b5d5e
Wanderson Domingos Lopes <wandersonlopes@gmail.com>
2022-09-09 22:29:58 -0300

    Update and rename Usando_vari_vel_Word_3_8_2015/Usando_vari_vel_Word_3_8_2015.ino to Usando_variavel_Word/Usando_variavel_Word.ino

---
+++ Usando_variavel_Word/Usando_variavel_Word.ino
@@ -0,0 +1,27 @@
+/*
+GME - Grupo de Metodos Eletroforéticos
+Instituto de Quimica - UFG
+
+software que utiiza a variavel word e biblioteca Serial
+
+criado   Aug 2015
+por Wanderson D. Lopes
+modificado set 2022
+por Wanderson D. Lopes
+*/
+
+
+
+void setup() {
+  word var = 16;
+  Serial.begin(9600);// put your setup code here, to run once:
+  Serial.print("O valor da variavel word é: ");
+  Serial.println(var);
+  Serial.print("A quantidade de bytes que ela ocupa é: ");
+  Serial.print(sizeof(var));
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
