ad7dca8a5f3eb4fe6bed83bf302243e471990050
Wanderson Domingos Lopes <wandersonlopes@gmail.com>
2022-09-15 20:01:10 -0300

    Update Prog_nivel_de_zero_e_deteccao.ino

--- Prog_nivel_de_zero_e_deteccao/Prog_nivel_de_zero_e_deteccao.ino
+++ Prog_nivel_de_zero_e_deteccao/Prog_nivel_de_zero_e_deteccao.ino
@@ -4,9 +4,38 @@ Instituto de Quimica - UFG
 
 O circuito ligado ao arduino é constituido por um fotodiodo polarizado apropriadamente com sinal ligado a porta A0 do Arduino.
 Também foi montado 3 transistores em paralelo devidamente polarizados ligados a um led. Esse Led quando alimentado devidamente fornece luz
-para o fotodiodo.
+para o fotodiodo. Os pinos ligados a esses transitores são o 8, 9 e 10.
 O codigo foi escrito para fazer a leitura do sinal do fotodiodo na porta A0 e ajuste de nível de zero pela Serial. 
 Através de comandos pela Serial realiza-se o acionamento de cada transitor em paralelo que alimenta o led.
+
+
+                                      +-----+
+         +----[PWR]-------------------| USB |--+
+         |                            +-----+  |
+         |         GND/RST2  [ ][ ]            |
+         |       MOSI2/SCK2  [ ][ ]  A5/SCL[ ] |   C5 
+         |          5V/MISO2 [ ][ ]  A4/SDA[ ] |   C4 
+         |                             AREF[ ] |
+         |                              GND[ ] |
+         | [ ]N/C                    SCK/13[ ] |   B5
+         | [ ]v.ref                 MISO/12[ ] |   .
+         | [ ]RST                   MOSI/11[ ]~|   .
+         | [ ]3V3    +---+               10[ ]~|   .
+         | [ ]5v     | A |                9[ ]~|   .
+         | [ ]GND   -| R |-               8[ ] |   B0
+         | [ ]GND   -| D |-                    |
+         | [ ]Vin   -| U |-               7[ ] |   D7
+         |          -| I |-               6[ ]~|   .
+         | [ ]A0    -| N |-               5[ ]~|   .
+         | [ ]A1    -| O |-               4[ ] |   .
+         | [ ]A2     +---+           INT1/3[ ]~|   .
+         | [ ]A3                     INT0/2[ ] |   .
+         | [ ]A4/SDA  RST SCK MISO     TX>1[ ] |   .
+         | [ ]A5/SCL  [ ] [ ] [ ]      RX<0[ ] |   D0
+         |            [ ] [ ] [ ]              |
+         |  UNO_R3    GND MOSI 5V  ____________/
+          \_______________________/
+		  
 
 criado   Set 2014
 por Wanderson D. Lopes
