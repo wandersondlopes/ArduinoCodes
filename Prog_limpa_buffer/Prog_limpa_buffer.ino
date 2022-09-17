3e41acce806c0245390d4d57edf1c063339c39fe
Wanderson Domingos Lopes <wandersonlopes@gmail.com>
2022-09-17 17:29:30 -0300

    Update Prog_limpa_buffer.ino

--- Prog_limpa_buffer/Prog_limpa_buffer.ino
+++ Prog_limpa_buffer/Prog_limpa_buffer.ino
@@ -1,3 +1,17 @@
+/*
+GME - Grupo de Metodos Eletroforéticos
+Instituto de Quimica - UFG
+
+Em algumas situações podemos ter a necessidade limpar o buffer responsável por armazenar dados da Serial
+O código a seguir apresenta uma maneira de remover esses dados do buffer
+
+criado   Nov 2015
+por Wanderson D. Lopes
+modificado set 2022
+por Wanderson D. Lopes
+*/
+
+
 void setup() {
   Serial.begin(9600);
 }
