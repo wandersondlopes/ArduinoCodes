51bc6cf0e7521394a0eed814f746abb8a4022258
Wanderson Domingos Lopes <wandersonlopes@gmail.com>
2022-09-21 15:41:36 -0300

    Update and rename Criacao_de_valor_randomico_analise_estatistica_04_07_2014/Criacao_de_valor_randomico_analise_estatistica_04_07_2014.ino to Valores_Randomicos_Distribuicao_E_Analise/Valores_Randomicos_Distribuicao_E_Analise.ino

---
+++ Valores_Randomicos_Distribuicao_E_Analise/Valores_Randomicos_Distribuicao_E_Analise.ino
@@ -0,0 +1,40 @@
+/*
+GME - Grupo de Metodos Eletroforéticos
+Instituto de Quimica - UFG
+
+
+
+criado   04/07/2014
+por Wanderson D. Lopes
+modificado set 2022
+por Wanderson D. Lopes
+*/
+
+
+long randomVal;
+ int saidadeloop = 0;
+ int n = 0;
+
+void setup(){
+  Serial.begin(9600);
+  randomSeed(analogRead(0));
+}
+
+void loop(){
+ 
+  while(saidadeloop == 0){
+    randomVal = random(300);
+    Serial.println(randomVal);
+    if (randomVal == 299){
+      n = n + 1;
+      Serial.print("veja ja saiu o valor maximo ");
+      Serial.print(n);
+      Serial.println("\t vezes");  
+    }
+    if (randomVal == 300){    
+      Serial.println("vai ate trezentos");
+      saidadeloop = 1;
+    }
+    delay(200);
+  }
+}
