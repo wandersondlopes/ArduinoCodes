22a80168b7c5fafdbeaf8b6f4412241d77154d60
Wanderson Domingos Lopes <wandersonlopes@gmail.com>
2022-11-10 11:54:47 -0300

    Add files via upload

---
+++ Receive_and_send_512_EEPROM/Receive_and_send_512_EEPROM.ino
@@ -0,0 +1,52 @@
+/* 
+
+
+Install LC_baseTools or Download https://github.com/leftCoast/LC_baseTools
+
+criado   5 de agosto 2020
+por jim lee
+modificado out 2022
+por Wanderson D. Lopes
+*/
+
+
+
+#include "textBuff.h"
+#include "timeObj.h"
+
+textBuff*   replyBuff;
+
+
+void setup(void) {
+
+   Serial.begin(9600);              // Or whatever speed you want.
+   replyBuff = new textBuff(80);    // Set the size of your text buffer.
+                                   // Watch out UNO users, you don't have a lot of RAM to play with.
+
+}
+
+
+// Send the text buffer out the serial port to see.
+void echoTheText(void) {
+
+   while(!replyBuff->empty()) {
+      Serial.print(replyBuff->readChar());
+   }
+}
+
+
+void loop(void) {
+
+  timeObj timer(10);
+
+  if (Serial.available()) {                        // If we have an incoming char..
+    timer.start();                                 // Start our timer.
+    while (!replyBuff->full()&&!timer.ding()) {    // While our buffer is NOT full and the timer has not expired..   
+      while (Serial.available()) {                 // While we have something to grab from the serial port..
+        replyBuff->addChar(Serial.read());         // Grab a char and stuff it into our buffer.
+        timer.start();                             // We got one, reset the timer.
+      }
+    }
+  }
+  echoTheText();                                   // If there's something in the buffer, lets see it!
+}
