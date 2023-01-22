8556b1b4b8a959a42a97a4680b588f119fdc41eb
Wanderson Domingos Lopes <wandersonlopes@gmail.com>
2023-01-22 19:27:43 -0300

    Update Sinal_Analogico_Arduino.ino

--- Sinal_Analogico_Arduino/Sinal_Analogico_Arduino.ino
+++ Sinal_Analogico_Arduino/Sinal_Analogico_Arduino.ino
@@ -1,34 +1,31 @@
-/*
+  /*
 
-O Arduino gera um sinal PWM que com uma shild adicional tem uma saida de sinal de onda senoidal
+  O Arduino gera um sinal PWM que com uma shild adicional tem uma saida de sinal de onda senoidal
 
-Criated Apr 2020
-by Lucas Alexandre
-Modified Jan 2023
-by Wanderson D. Lopes
-*/
+  Criated Apr 2020
+  by Lucas Alexandre
+  Modified Jan 2023
+  by Wanderson D. Lopes
+  */
 
+  // Sine wave generator simulator
 
+  int outputPWM;
+  int x, valor;
+  float seno;
 
+  #define outputPWM 11
 
-// Sine wave generator simulator
- 
- int outputPWM;
- int x, valor;
- float seno;
- 
- #define outputPWM 11
- 
- void setup() {
-  pinMode (outputPWM, OUTPUT);
- 
- }
- 
- void loop () {
-  for (x=0; x<255; x++) {
-  seno = 100+(100*(sin(x*(6.28/255))));
-  valor = int(seno);
-  analogWrite(outputPWM, valor);
-  delay(30);
-  }
- }
+  void setup(){
+  pinMode (outputPWM, OUTPUT);
+
+  }
+
+  void loop (){
+    for (x=0; x<255; x++){
+    seno = 100+(100*(sin(x*(6.28/255))));
+    valor = int(seno);
+    analogWrite(outputPWM, valor);
+    delay(30);
+    }
+  }
