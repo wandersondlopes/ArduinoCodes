cb2ef565e1c1c0965fffa444d066725ceb42c931
Wanderson Domingos Lopes <wandersonlopes@gmail.com>
2023-02-23 00:19:06 -0300

    Add files via upload

---
+++ SI5351_clock0_and_clock1.ino
@@ -0,0 +1,57 @@
+/*
+
+Exemple set clock0 and clock1 output using Si5351Arduino library
+
+Criated 2015
+by Jason Milldrum <milldrum@gmail.com>
+Modified Feb 2023
+by Wanderson D. Lopes
+*/
+
+#include "si5351.h"
+#include "Wire.h"
+
+Si5351 si5351;
+
+void setup()
+{
+  bool i2c_found;
+
+  // Start serial and initialize the Si5351
+  Serial.begin(57600);
+  i2c_found = si5351.init(SI5351_CRYSTAL_LOAD_8PF, 0, 0);
+  if(!i2c_found)
+  {
+    Serial.println("Device not found on I2C bus!");
+  }
+
+  // Set CLK0 to output 14 MHz
+  si5351.set_freq(1000000000ULL, SI5351_CLK0);
+
+  // Set CLK1 to output 100 MHz
+  si5351.set_ms_source(SI5351_CLK1, SI5351_PLLB);
+  si5351.set_freq_manual(10000000000ULL, 70000000000ULL, SI5351_CLK1);
+
+  // Query a status update and wait a bit to let the Si5351 populate the
+  // status flags correctly.
+  si5351.update_status();
+  delay(500);
+}
+
+void loop()
+{
+  // Read the Status Register and print it every 10 seconds
+  si5351.update_status();
+  Serial.print("SYS_INIT: ");
+  Serial.print(si5351.dev_status.SYS_INIT);
+  Serial.print("  LOL_A: ");
+  Serial.print(si5351.dev_status.LOL_A);
+  Serial.print("  LOL_B: ");
+  Serial.print(si5351.dev_status.LOL_B);
+  Serial.print("  LOS: ");
+  Serial.print(si5351.dev_status.LOS);
+  Serial.print("  REVID: ");
+  Serial.println(si5351.dev_status.REVID);
+
+  delay(10000);
+}
