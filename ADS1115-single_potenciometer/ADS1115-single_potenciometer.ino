a91d3a179f42180defe90d1a7e349bc424e9d247
Wanderson Domingos Lopes <wandersonlopes@gmail.com>
2023-05-10 02:55:02 -0300

    Add files via upload

---
+++ ADS1115-single_potenciometer/ADS1115-single_potenciometer.ino
@@ -0,0 +1,54 @@
+/*
+Aplicação simples de impressão na Serial de valores medidos pelo conversor analógico digital ADS1115.
+Foi utilizado um potenciometro ligado ao ADS1115.
+Teste de funcionamento
+Library code found at: https://github.com/thingSoC/TSOC_GROVEY_ADC/blob/master/libraries/Adafruit_ADS1X15/Adafruit_ADS1015.h
+
+criado     Abr 2021
+por Wanderson D. Lopes
+modificado Out 2022
+por Wanderson D. Lopes
+*/
+
+
+
+#include <Wire.h>
+#include <Adafruit_ADS1015.h>
+
+Adafruit_ADS1115 ads(0x48);  /* ADC object at I2C address 0x48 for addr pin = GND */
+// Adafruit_ADS1015 ads;     /* 12-bit version */
+
+void setup(void) 
+{
+  Serial.begin(9600);
+  Serial.println("Hello!");
+  
+  Serial.println("Getting single-ended readings from AIN0..3");
+  Serial.println("ADC Range: +/- 6.144V (1 bit = 3mV/ADS1015, 0.1875mV/ADS1115)");
+  
+  // The ADC input range (or gain) can be changed via the following
+  // functions, but be careful never to exceed VDD +0.3V max, or to
+  // exceed the upper and lower limits if you adjust the input range!
+  // Setting these values incorrectly may destroy your ADC!
+  //                                                                ADS1015  ADS1115
+  //                                                                -------  -------
+  // ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
+  // ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
+  // ads.setGain(GAIN_TWO);        // 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
+  // ads.setGain(GAIN_FOUR);       // 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
+  // ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
+  // ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
+  
+  ads.begin(); 
+}
+
+void loop(void) 
+{
+  int16_t adc0;
+//int16_t adc1, adc2, adc3;
+
+  adc0 = ads.readADC_SingleEnded(0);
+  Serial.print("AIN0: "); 
+  Serial.println(adc0);  
+  delay(1000);
+}
