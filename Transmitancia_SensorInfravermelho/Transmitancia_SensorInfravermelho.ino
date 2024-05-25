ecc8b251ee77557763192ccbff25a20975b9ef73
Wanderson Domingos Lopes <wandersonlopes@gmail.com>
2024-05-25 01:08:08 -0300

    Update Transmitancia_SensorInfravermelho.ino

--- Transmitancia_SensorInfravermelho/Transmitancia_SensorInfravermelho.ino
+++ Transmitancia_SensorInfravermelho/Transmitancia_SensorInfravermelho.ino
@@ -11,14 +11,6 @@ por Wanderson D. Lopes
 */
 
 
-
-
-
-/*
-
-
-*/
-
 int sensorPin = A0;    // select the input pin for the potentiometer
 int sensorValue = 0;  // variable to store the value coming from the sensor
 bool Cabecalio = true;
